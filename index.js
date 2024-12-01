3102dea8abda381641c758b82fa9f05252eda78c
replakcan <alper.makcan@gmail.com>
2024-12-02 00:34:41 +0300

    Updates index.js w/week4 contents

--- index.js
+++ index.js
@@ -1,84 +1,42 @@
-const Person = require("./person");
-const Pet = require("./pet");
-const AnimalShelter = require("./animalShelter");
-const Database = require("./database");
+const express = require("express");
 const PersonService = require("./services/person-service");
-const ShelterService = require("./services/shelter-service");
-const personService = require("./services/person-service");
+const bodyParser = require("body-parser");
 
-/*
-    There should be a person who can go and adopt a pet from a shelter,
-    person can have multiple pets and person can name their pets,
-    pets should have breed and age for default,
-    shelter should have a name and a petCount and a list of pets,
-*/
+const app = express();
 
-/* const alper = new Person("Alper", 28);
-const mutlu = new Person('Mutlu', 29);
+app.use(bodyParser.urlencoded());
 
-const people = [];
-people.push(alper);
-people.push(mutlu);
+app.use(bodyParser.json());
+app.set("view engine", "pug");
 
-const dog = new Pet("dog", 3);
-const cat = new Pet("cat", 5);
-const dog1 = new Pet("doge", 9);
+app.get("/", (req, res) => {
+  // res.send('Hello ')
+  // res.sendFile(__dirname + "/index.html");
+  res.render("index");
+});
 
-const shelterIstanbul = new AnimalShelter("Istanbul", [dog, cat, dog1]);
+app.get("/person/all", async (req, res) => {
+  const people = await PersonService.findAll();
+  res.render("person", { people: people });
+});
 
-alper.adoptPet(shelterIstanbul);
-alper.adoptPet(shelterIstanbul);
-alper.adoptPet(shelterIstanbul);
-alper.adoptPet(shelterIstanbul);
+app.get("/person/:id", async (req, res) => {
+  const id = req.params.id;
+  const person = await PersonService.find(id);
+  res.send(person);
+});
 
-alper.namePet(alper.pets[1], "Susa");
+app.post("/person", async (req, res) => {
+  const person = await PersonService.add(req.body);
+  res.send(person);
+});
 
-alper.playWithPet(alper.pets[1]);
-console.log(alper.pets); */
+app.delete("/person/:id", async (req, res) => {
+  const id = req.params.id;
+  await PersonService.del(id);
+  res.send('ok');
+});
 
-/* Database.save('person.json', people);
-Database.save('shelter.json', shelterIstanbul); */
-
-/* const cbFunc = (err, file) => {
-  if (err) {
-    console.log("errorumsumide: ", err);
-    return;
-  } else {
-    console.log("FILE: ", file);
-
-    const alper = Person.create(file[0]);
-    console.log("alper: ", alper.pets[1]);
-    alper.playWithPet(alper.pets[0]);
-    console.log(alper.pets[0]);
-    alper.pets[1].talk();
-  }
-};
-Database.load("person.json", cbFunc);
-
-const alfonzo = new Person("alfonso", 23, [], 1);
-console.log("alfonso: ", alfonzo); */
-
-async function main() {
-  const susa = new Pet("cat", 5, 1);
-  const zorro = new Pet("dog", 12, 2);
-
-  const shelterIst = new AnimalShelter("Istanbul", [susa, zorro], 1);
-
-  const alper = new Person("Alper", 28, [], 1);
-  const mutlu = new Person("Mutlu", 20, [], 2);
-
-  alper.adoptPet(shelterIst);
-  alper.adoptPet(shelterIst);
-
-  alper.namePet(susa, "susa");
-  alper.namePet(zorro, "zorro");
-
-  alper.playWithPet(susa);
-  alper.playWithPet(zorro);
-
-  const people = await PersonService.findAll()
-
-  await PersonService.del(9)
-}
-
-main()
+app.listen(3000, () => {
+  console.log("Server listening");
+});
