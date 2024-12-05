33715ea3fa24ddfc64dcadc3e6af8a9b1de30528
replakcan <alper.makcan@gmail.com>
2024-12-05 16:28:20 +0300

    Purifies index.js and creates app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const personRouter = require("./routes/person");
+const petRouter = require("./routes/pet");
+const animalShelterRouter = require("./routes/animalShelter");
+
+require("./mongo-connection");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/person", personRouter);
+app.use("/pet", petRouter);
+app.use("/shelter", animalShelterRouter);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = app;
